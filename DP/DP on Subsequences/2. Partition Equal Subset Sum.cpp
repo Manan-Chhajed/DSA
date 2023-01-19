53221d06756f7a2fbf687d065d44b7c663091a83
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-19 14:52:19 +0530

    Create 2. Partition Equal Subset Sum.cpp

---
+++ DP/DP on Subsequences/2. Partition Equal Subset Sum.cpp
@@ -0,0 +1,3 @@
+// https://www.codingninjas.com/codestudio/problems/partition-equal-subset-sum_892980?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=1
+
+// just a little variation of the prev one.
