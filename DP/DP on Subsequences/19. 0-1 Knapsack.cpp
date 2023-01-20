ea2967742f8b711b1081b46b26d2f6e8415e8a2b
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-20 18:10:20 +0530

    Create 19. 0-1 Knapsack.cpp

---
+++ DP/DP on Subsequences/19. 0-1 Knapsack.cpp
@@ -0,0 +1,16 @@
+// https://www.codingninjas.com/codestudio/problems/0-1-knapsack_920542?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+
+/* 
+  1. Greedy won't work as uniformity not there
+  2. So try all combinations, recursion
+  
+  // Memoization TC O(N * W)
+  // SC O(N * W) + auxillary stack space O(N)
+
+  // Tabulation TC same
+  // SC O(N * W)
+  
+  // Space Optimization --> SC reduces to O(2 * maxWeight)
+  
+  // further space optimization O(maxWeight)
+*/
