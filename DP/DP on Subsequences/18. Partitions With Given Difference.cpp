afb0e9f1ac7802351664776fc814849c5ab7b6f1
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-20 14:56:50 +0530

    Update and rename 18. Partitions With Given Difference to 18. Partitions With Given Difference.cpp

---
+++ DP/DP on Subsequences/18. Partitions With Given Difference.cpp
@@ -0,0 +1,11 @@
+// https://www.codingninjas.com/codestudio/problems/partitions-with-given-difference_3751628?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+
+/* 
+  2 subsets s1 and s2 && s1 > s2 
+  so s1 = totalSum - s2
+  we have been asked s1 - s2 = d
+  so totalSum - s2 - s2 = d;
+  s2 = (totalSum - d) / 2
+  
+  now same as previous question
+*/
