620659c5e990aad152f0a6c158fc5f19931cb571
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-20 18:59:51 +0530

    Create 20. Minimum Coins.cpp

---
+++ DP/DP on Subsequences/20. Minimum Coins.cpp
@@ -0,0 +1,8 @@
+// https://www.codingninjas.com/codestudio/problems/minimum-elements_3843091?leftPanelTab=0
+
+/* 
+  Greedy fails --> just think of a case where it fails
+  0/1 Knapsack is the way to go
+  
+  Only catch is that here we have infinite supply of coins of a particular value
+*/
