570fef44aced5c150e1dd6a43319a5bad11b8456
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-21 10:56:00 +0530

    Update 21. Target Sum.cpp

--- DP/DP on Subsequences/21. Target Sum.cpp
+++ DP/DP on Subsequences/21. Target Sum.cpp
@@ -1,1 +1,8 @@
+// https://www.codingninjas.com/codestudio/problems/target-sum_4127362?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
 
+/* 
+  Same as subset with a given difference
+  1. 1st subset is the +ve one
+  2. 2nd is the -ve one
+  done previously
+*/
