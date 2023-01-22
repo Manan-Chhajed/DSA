13dac1aa8528155c2e9e5f18eb6556f4ae0da698
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-22 17:07:45 +0530

    Update 25. Longest Common Subsequence.cpp

--- DP/DP on Strings/25. Longest Common Subsequence.cpp
+++ DP/DP on Strings/25. Longest Common Subsequence.cpp
@@ -1,1 +1,20 @@
+// https://www.codingninjas.com/codestudio/problems/longest-common-subsequence_624879?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
 
+// To print all subsequences --> Power Set
+//                           --> Recursion   
+
+/*
+  here we will need 2 indices to represent the recursion
+  previously pick not pick, here match not match
+  
+  // recursive approach --> exponential TC
+
+  // lets opitmize. Overlapping subproblems?? Yeah memoize
+  // for memoization --> TC O(n1 * n2)
+  //                 --> SC O(n1 * n2) + O(n1 + n2) auxillary stack space
+  
+  // tabulation 
+  // 1. copy the base case
+  // 2. write down the changing parameters in opp. order
+  // 3. copy the recurrence
+*/
