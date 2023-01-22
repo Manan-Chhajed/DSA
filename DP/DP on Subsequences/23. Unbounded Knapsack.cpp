29e2cdc1f7105d15dbe5eed87e0ae2bd580009f1
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-22 12:28:18 +0530

    Create 23. Unbounded Knapsack.cpp

---
+++ DP/DP on Subsequences/23. Unbounded Knapsack.cpp
@@ -0,0 +1,7 @@
+// different from 0-1 knapsack in the sense that we have infinite supply
+// https://www.codingninjas.com/codestudio/problems/unbounded-knapsack_1215029?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+
+// for recursion and memo
+// auxillary stack space = O(w)
+
+// Space optimization in tabulation possible if there is something like index and index - 
