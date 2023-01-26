e46825aa5af7a6a5e945cbf65fa62e658ba32237
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-26 16:06:17 +0530

    Create 31. Shortest Common Supersequence.cpp

---
+++ DP/DP on Strings/31. Shortest Common Supersequence.cpp
@@ -0,0 +1,8 @@
+// https://www.codingninjas.com/codestudio/problems/shortest-supersequence_4244493?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+
+// finding length is easy
+// len = n + m - length of LCS
+
+// how to figure out the super string
+// watch the video
+
