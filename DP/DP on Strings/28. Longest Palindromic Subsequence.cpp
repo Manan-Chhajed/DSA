8767dfb7519cde88036aee9a6535c2cfd2a8f2d2
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-26 13:33:05 +0530

    Update 28. Longest Palindromic Subsequence.cpp

--- DP/DP on Strings/28. Longest Palindromic Subsequence.cpp
+++ DP/DP on Strings/28. Longest Palindromic Subsequence.cpp
@@ -1,4 +1,4 @@
-// https://www.codingninjas.com/codestudio/problems/longest-common-subsequence_624879?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+// https://www.codingninjas.com/codestudio/problems/longest-palindromic-subsequence_842787?leftPanelTab=0
 
 /* 
   longest palindromic subsequence is basically the longest common subsequence LCS of string s1 and reverse of s1
