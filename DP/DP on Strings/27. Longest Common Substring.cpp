58b63214eaf53d0d71a4c17cc1cee4fcce1081ea
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-26 13:22:53 +0530

    Update 27. Longest Common Substring.cpp

--- DP/DP on Strings/27. Longest Common Substring.cpp
+++ DP/DP on Strings/27. Longest Common Substring.cpp
@@ -1,2 +1,3 @@
 // https://www.codingninjas.com/codestudio/problems/longest-common-substring_1235207?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
 
+// let's directly write tabulation because in recursion there will be 3 variables and a little complex
