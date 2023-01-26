4e5e32f7bcdac6174ae0604044101459df2bc5ef
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-01-26 14:42:10 +0530

    Create 30. Minimum Insertion Deletions to Convert String A to String B.cpp

---
+++ DP/DP on Strings/30. Minimum Insertion Deletions to Convert String A to String B.cpp
@@ -0,0 +1,6 @@
+// https://www.codingninjas.com/codestudio/problems/can-you-make_4244510?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+
+// don't touch the longest common subsequence of the 2 strings
+// deletions = n - length of LCS
+// insertions = m - length of LCS
+// so final ans = n + m - 2 * length of LCS
