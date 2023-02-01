a19b7d96e7499dc48e23b12fc50f0de79513c8c5
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-02-01 19:23:02 +0530

    Create 36. Buy and Sell Stock - II.cpp

---
+++ DP/DP on Stocks/36. Buy and Sell Stock - II.cpp
@@ -0,0 +1,26 @@
+// https://www.codingninjas.com/codestudio/problems/selling-stock_630282?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos
+
+/*
+  Here you can buy and sell multiple times
+  
+  1. Express in terms of f(ind, buy)
+  2. Explore possiblilites on that day
+  3. Take max profit
+  4. Base case
+  
+  Recursion 
+  TC --> O(2 ^ n)
+  SC --> O(n)
+  
+  Memoization
+  TC --> O(n * 2)
+  SC --> O(n * 2) + auxillary stack space O(n)
+  
+  Tabulation
+  TC --> O(n * 2)
+  SC --> O(n * 2)
+  
+  Space Optimization
+  TC --> O(n * 2)
+  SC --> O(2 * 2)
+*/
