31d652e7cce77a0281ddf253611ca309bc65fe25
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-02-25 19:46:26 +0530

    Update 1. intro.cpp

--- Trie/1. intro.cpp
+++ Trie/1. intro.cpp
@@ -5,5 +5,13 @@
   
   https://www.codingninjas.com/codestudio/problems/implement-trie_1387095
   
+  https://www.codingninjas.com/codestudio/problems/complete-string_2687860?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_tries_videos
+  
+  https://www.codingninjas.com/codestudio/problems/count-distinct-substrings_985292?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_tries_videos
+  
+  Bit Manipulation basics
+  
+  https://www.codingninjas.com/codestudio/problems/maximum-xor_973113?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_tries_videos
+  
   
 */
