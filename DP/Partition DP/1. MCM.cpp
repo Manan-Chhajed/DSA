6510fb007aa661f82f6949adb8bf6ff81716efd7
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-03-12 18:22:49 +0530

    Update 1. MCM.cpp

--- DP/Partition DP/1. MCM.cpp
+++ DP/Partition DP/1. MCM.cpp
@@ -1,1 +1,21 @@
+// Partition DP
+/*
+  e.g. when A * B * C
+  --> (A * B) * C
+  --> A * (B * C)
+  since multiple ways, partition DP
+*/
 
+// 1. Matrix Chain Multiplication
+/*
+   - start with entire block/array
+   - try all partitions
+   - return best possible 2 partitions
+   
+   Memoization
+   - TC O(n * n * n)
+   - SC O(n * n) + O(n)
+   
+   Tabulation
+   - 
+*/
